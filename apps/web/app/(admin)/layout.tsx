81c117bf0940e635d1a6b3fb8ed9a2a8319f2eb4
IamShaDoW666 <www.gaming2018@gmail.com>
2025-10-27 16:13:48 +0530

    Add Api keys

--- apps/web/app/(admin)/layout.tsx
+++ apps/web/app/(admin)/layout.tsx
@@ -7,30 +7,28 @@ import { redirect } from "next/navigation"
 import { UserProvider } from "@/context/UserContext"
 
 export default async function DashboardLayout({
-  children,
+    children,
 }: {
-  children: React.ReactNode
+    children: React.ReactNode
 }) {
-  const session = await auth.api.getSession({
-    headers: await headers()
-  })
+    const session = await auth.api.getSession({
+        headers: await headers()
+    })
+    if (!session) {
+        return redirect("/sign-in")
+    }
 
-    console.log(JSON.stringify(session, null, 2));
-  if (!session) {
-    return redirect("/sign-in")
-  }
-
-  const user = session?.user;
-  return (
-    <UserProvider user={user}>
-      <SidebarProvider>
-        <AppSidebar variant="inset" />
-        <SidebarInset>
-          <AppHeader user={user} />
-          <main className="flex-1 p-6">{children}</main>
-        </SidebarInset>
-      </SidebarProvider>
-    </UserProvider>
-  )
+    const user = session?.user;
+    return (
+        <UserProvider user={user}>
+            <SidebarProvider>
+                <AppSidebar variant="inset" />
+                <SidebarInset>
+                    <AppHeader user={user} />
+                    <main className="flex-1 p-6">{children}</main>
+                </SidebarInset>
+            </SidebarProvider>
+        </UserProvider>
+    )
 }
 
--- apps/web/app/(admin)/settings/api-keys/page.tsx
+++ apps/web/app/(admin)/settings/api-keys/page.tsx
@@ -1,19 +1,15 @@
 import { auth } from "@/lib/auth";
 import { headers } from "next/headers";
 import { redirect } from "next/navigation";
+import { ApiKeysPage } from "./client";
 
-export default async function SettingsAdminLayout({
-    children,
-}: {
-    children: React.ReactNode;
-}) {
+export default async function Page() {
     const session = await auth.api.getSession({
         headers: await headers(),
     });
-
     if (!session) {
         return redirect("/sign-in");
     }
 
-    return children;
+    return <ApiKeysPage />;
 }
