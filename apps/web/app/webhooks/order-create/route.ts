e0b1375881968ec3456195d418a09bdf458a84b3
IamShaDoW666 <www.gaming2018@gmail.com>
2026-04-06 20:01:01 +0530

    update order-create webhook

--- apps/web/app/webhooks/order-create/route.ts
+++ apps/web/app/webhooks/order-create/route.ts
@@ -9,7 +9,6 @@ export const config = {
     },
 };
 
-
 export async function POST(request: Request) {
     const rawBody = Buffer.from(await request.arrayBuffer());
     const body = JSON.parse(rawBody.toString());
@@ -41,7 +40,6 @@ export async function POST(request: Request) {
         const isValid = verifyHMAC(rawBody, shopifyKey, user.shopifyKey)
         console.log("VALIDITY: ", isValid, shopifyDomain)
         const svc = new MessageService(user.Device[0].body, user.id)
-
         let content = "Hi Thank you for Shopping with us! This is a test message"
         if (user.shopifyTemplate) {
             const { parseTemplate } = await import('@/lib/templateParser')
@@ -51,8 +49,31 @@ export async function POST(request: Request) {
         console.log("Number: ", body.shipping_address?.phone || phone)
         // const targetPhone = body.shipping_address?.phone || phone || "+917902708908";
         const targetPhone = "+917902708908";
+        // const STATIC_LOGO_URL = "https://www.linvecreations.com/cdn/shop/files/linve.png?v=1774507969&width=165"; // Replace with your actual logo URL
+        const STATIC_LOGO_URL = "https://cdn.shopify.com/s/files/1/0721/5146/6041/files/linvered.png?v=1775490870"; // Replace with your actual logo URL
 
-        await svc.queueSendMessage(targetPhone, content)
+        if (body.order_status_url) {
+            await svc.queueButtonMessage(
+                targetPhone,
+                content,
+                content,
+                "Order Confirmation",
+                "Keep tracking your order",
+                [
+                    {
+                        name: "cta_url",
+                        buttonParamsJson: JSON.stringify({
+                            display_text: "View Order",
+                            url: body.order_status_url
+                        })
+                    }
+                ],
+                STATIC_LOGO_URL,
+                { mediaType: 'image' }
+            );
+        } else {
+            await svc.queueSendMessage(targetPhone, content);
+        }
         return NextResponse.json({ message: 'Message queued successfully' }, { status: 200 })
 
     } else {
@@ -61,3 +82,4 @@ export async function POST(request: Request) {
 
     return NextResponse.json(body)
 }
+
